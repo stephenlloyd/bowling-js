fd8a07df41f3f2bebc71da06ebe796987a27374f
Stephen Lloyd <stephen.lloyd85@gmail.com>
2015-06-01 10:09:00 +0100

    Passed Frame test: knows when it is / is not over after 2 / less than 2 goes.

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -5,9 +5,29 @@ describe("Frame", function() {
     frame = new Frame();
   });
 
-  it("can register a go", function() {
+  it("registers a gutter ball", function() {
     frame.registerGo(0);
     expect(frame.remainingPins()).toEqual(10);
   });
 
+  it("registers a successful roll", function() {
+    frame.registerGo(5);
+    expect(frame.remainingPins()).toEqual(5);
+  });
+
+  it("can register 2 rolls", function(){
+    frame.registerGo(5);
+    frame.registerGo(4);
+    expect(frame.remainingPins()).toEqual(1);
+  });
+
+  it("knows when it is over after two rolls", function() {
+    frame.registerGo(1);
+    frame.registerGo(1);
+    expect(frame.isOver()).toEqual(true);
+  });
+
+  it("knows when it is not over after less than two rolls", function(){
+    expect(frame.isOver()).toEqual(false);
+  })
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,10 +1,18 @@
 function Frame() {
+  this.goCount = 2;
+  this.standingPins = 10;
 }
 
 Frame.prototype.registerGo = function(pinCount) {
+  this.goCount -= 1;
+  this.standingPins -= pinCount;
 }
 
 Frame.prototype.remainingPins = function() {
-  return 10;
+  return this.standingPins;
+};
+
+Frame.prototype.isOver = function() {
+  return (this.goCount === 0);
 };
 
